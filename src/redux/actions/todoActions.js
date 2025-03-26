c526d8dd5902f9a5536b664cad2fae525c71be10
Divynshi-Singh <divya.singh@innotechteam.in>
2025-03-26 18:50:39 +0530

    fixed PR comments

--- src/redux/actions/todoActions.js
+++ src/redux/actions/todoActions.js
@@ -46,3 +46,4 @@ export const updateAlarmStatus = () => (dispatch, getState) => {
     payload: updatedTodos,
   });
 };
+
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,7 +1,6 @@
 import { ADD_TODO, EDIT_TODO, DELETE_TODO, TOGGLE_TODO, UPDATE_ALARM_STATUS } from '../types';
 import { fetchTodoStatusColor } from '../../utils/utils';
-const initialState = JSON.parse(localStorage.getItem('todos')) || { todos: [] };
-
+const initialState = JSON.parse(localStorage.getItem('todos')) || [];
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
